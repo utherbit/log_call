15043ce5ca08acd51577f52fccbf70b73b406851
utherbit <78534611+utherbit@users.noreply.github.com>
2023-03-20 13:52:03 +0300

    Update logs.go

--- logs.go
+++ logs.go
@@ -2,37 +2,120 @@ package log_call
 
 import (
 	"fmt"
-	"log"
+	"time"
 )
 
-var LogCall = logCallHandlerGroups{state: make(map[string]*logCallHandler)}
+var LogCall = LogCallHandlerGroups{state: make(map[string]*LogCallHandler), cfg: cfgDefault()}
 
-type logCallHandlerGroups struct {
-	state map[string]*logCallHandler
+type LogCallHandlerGroups struct {
+	cfg   Config
+	state map[string]*LogCallHandler
 }
 
-type logCallHandler struct {
+const (
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorBlue   = "\033[34m"
+	ColorPurple = "\033[35m"
+	ColorCyan   = "\033[36m"
+	ColorWhite  = "\033[37m"
+
+	ColorReset = "\033[0m"
+)
+
+func cfgDefault() Config {
+	return Config{
+		Display: &ConfigDisplay{
+			Time:    true,
+			Group:   true,
+			Message: true,
+		},
+		ColorsTime:    ColorGreen,
+		ColorsGroup:   ColorBlue,
+		ColorsMessage: ColorWhite,
+
+		TimeFormat: "_2 Jan 15:04:05",
+	}
+}
+
+type ConfigDisplay struct {
+	Time    bool // Выключение лога для времени
+	Group   bool // Выключение лога для группы
+	Message bool // Выключение лога для сообщения
+}
+type Config struct {
+	Groups map[string]bool // Выключение логов для перечисленных групп
+
+	Display *ConfigDisplay
+	//DisplayTime    bool // Выключение лога для времени
+	//DisplayGroup   bool // Выключение лога для группы
+	//DisplayMessage bool // Выключение лога для сообщения
+
+	TimeFormat string
+
+	ColorsTime    string // Цвет лога для времени
+	ColorsGroup   string // Цвет лога для группы
+	ColorsMessage string // Цвет лога для сообщения
+
+}
+
+type LogCallHandler struct {
+	root  *LogCallHandlerGroups
 	group string
 	state bool
 }
 
-func (b logCallHandler) Log(i interface{}) {
-	fmt.Printf("Log, b %#v", b.state)
-
+func (b LogCallHandler) Log(i ...interface{}) {
 	if b.state {
-		log.Println(b.group, ": g", i)
+		print("\n")
+		// 20 Mar 12:50:23
+		if b.root.cfg.Display.Time {
+			print(b.root.cfg.ColorsTime, time.Now().Format(b.root.cfg.TimeFormat))
+		}
+
+		// Authorization:
+		if b.root.cfg.Display.Group {
+			print(b.root.cfg.ColorsGroup, " ", b.group, ": ")
+		}
+
+		// HandlerLogin
+		print(b.root.cfg.ColorsMessage)
+		fmt.Print(i...)
+
+		// the end, set default color cursor
+		print(ColorReset)
 	}
 }
 
-func (g *logCallHandlerGroups) SetConfig(config map[string]bool) {
-	for s, b := range config {
-		g.state[s] = &logCallHandler{
+func (g *LogCallHandlerGroups) SetConfig(config Config) {
+	if config.ColorsTime != "" {
+		g.cfg.ColorsTime = config.ColorsTime
+	}
+	if config.ColorsGroup != "" {
+		g.cfg.ColorsGroup = config.ColorsGroup
+	}
+	if config.ColorsMessage != "" {
+		g.cfg.ColorsMessage = config.ColorsMessage
+	}
+	if config.TimeFormat != "" {
+		g.cfg.TimeFormat = config.TimeFormat
+	}
+	if config.Display != nil {
+		g.cfg.Display = config.Display
+	}
+
+	g.cfg.Groups = config.Groups
+
+	for s, b := range config.Groups {
+		g.state[s] = &LogCallHandler{
 			group: s,
 			state: b,
 		}
 	}
 }
-func (g logCallHandlerGroups) AddGroup(group string, state ...bool) *logCallHandler {
+func (g *LogCallHandlerGroups) AddGroup(group string, state ...bool) *LogCallHandler {
+
 	if h, ok := g.state[group]; ok {
 		if len(state) > 0 {
 			h.state = state[0]
@@ -40,11 +123,14 @@ func (g logCallHandlerGroups) AddGroup(group string, state ...bool) *logCallHand
 		return h
 	}
 
-	h := &logCallHandler{
+	h := &LogCallHandler{
+		root:  g,
 		group: group,
 	}
 	if len(state) > 0 {
 		h.state = state[0]
+	} else {
+		h.state = true
 	}
 	g.state[group] = h
 	return h
